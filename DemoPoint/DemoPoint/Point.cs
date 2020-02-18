61cca4a0ca194d7f1ded329d1cfac94de07e5300
Nico De Witte <nico.dewitte@outlook.com>
2020-02-18 08:46:28 +0100

    Add demo of point class project

---
+++ DemoPoint/DemoPoint/Point.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoPoint
+{
+    public class Point
+    {
+        // Constructor
+        public Point(double x, double y)
+        {
+            Move(x, y);
+        }
+
+        // Default constructor = no arguments
+        public Point()
+        {}
+
+        public void Move(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double GetX()        // Getter for x
+        {
+            return x;
+        }
+
+        public double GetY()        // Getter for y
+        {
+            return y;
+        }
+
+        public void SetX(double x)      // Setter for x
+        {
+            this.x = x;
+        }
+
+        public void SetY(double y)      // Setter for y
+        {
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({x},{y})";
+        }
+
+        // Attributes
+        private double x = 0;
+        private double y = 0;
+    }
+}
---
+++ DemoPoint/DemoPoint/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DemoPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point center = new Point();     // Default constructor
+
+            Console.WriteLine("Center: " + center);
+
+            center.Move(42, 42);
+            Console.WriteLine("Moved Center: " + center);
+
+            Point location = new Point(14, 32);
+            Console.WriteLine("Location: " + location);
+        }
+    }
+}
